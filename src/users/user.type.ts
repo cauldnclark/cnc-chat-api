d04c8340c252d6e98da6a1e571e5bc1a9941f011
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2021-06-02 17:35:10 +0800

    adding UserPublic type

--- src/users/user.type.ts
+++ src/users/user.type.ts
@@ -20,3 +20,21 @@ export class UserType {
   @Field()
   password: string;
 }
+
+@ObjectType('UserPublic')
+export class UserPublicType {
+  @Field((type) => ID)
+  _id: string;
+
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  username: string;
+}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,30 +1,30 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { CreateUserInput, UpdateUserInput, User } from './user.entity';
-import { UserType } from './user.type';
+import { UserPublicType, UserType } from './user.type';
 import { UsersService } from './users.service';
 
 @Resolver((of) => UserType)
 export class UsersResolver {
   constructor(private userService: UsersService) {}
 
-  @Query((returns) => UserType)
+  @Query((returns) => UserPublicType)
   async user(@Args('_id') _id: string): Promise<User> {
     return await this.userService.getUser(_id);
   }
 
-  @Query((returns) => [UserType])
+  @Query((returns) => [UserPublicType])
   async users(): Promise<User[]> {
     return this.userService.getUsers();
   }
 
-  @Mutation((returns) => UserType)
+  @Mutation((returns) => UserPublicType)
   async signUp(
     @Args('createUserInput') createUserInput: CreateUserInput,
   ): Promise<User> {
     return await this.userService.create(createUserInput);
   }
 
-  @Mutation((returns) => UserType)
+  @Mutation((returns) => UserPublicType)
   async updateUser(
     @Args('updateUserInput') updateUserInput: UpdateUserInput,
     @Args('userId') userId: string,
@@ -32,7 +32,7 @@ export class UsersResolver {
     return await this.userService.updateUser(updateUserInput, userId);
   }
 
-  @Mutation((returns) => UserType)
+  @Mutation((returns) => UserPublicType)
   async deleteUser(@Args('userId') userId: string): Promise<User> {
     return this.userService.deleteUser(userId);
   }
